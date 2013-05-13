c76a23f73da0f162fbcc4ee3c39cdf76c9123201
Bryan Ching <bryan792@gmail.com>
2013-05-13 14:22:29 -0700

    Maximal sequence found but not printed

--- src/NaiveSuffixTree.java
+++ src/NaiveSuffixTree.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class NaiveSuffixTree
 {
@@ -16,14 +18,13 @@ public class NaiveSuffixTree
       children = new ArrayList<Node>();
       leaves = new ArrayList<Integer>();
     }
-    
+
     public Node(int start, int end)
     {
       this.stringStart = start;
       this.stringEnd = end;
       children = new ArrayList<Node>();
       leaves = new ArrayList<Integer>();
-      this.leaf = leaf;
     }
 
     public Node(int start, int end, int leaf)
@@ -36,6 +37,11 @@ public class NaiveSuffixTree
       this.leaf = leaf;
     }
 
+    public int length()
+    {
+      return stringEnd - stringStart;
+    }
+
     public void addChild(Node node)
     {
       children.add(node);
@@ -44,24 +50,27 @@ public class NaiveSuffixTree
 
   private Node root;
   public String input;
+  private ArrayList<Node> nodes;
 
   public NaiveSuffixTree()
   {
     root = new Node();
     root.count--;
+    nodes = new ArrayList<Node>();
   }
 
   int leaf = 1;
 
   public Node createTree(String input)
   {
-    this.input = input.concat("$");
+    this.input = input;
+    // this.input = input.concat("$");
     // this.input = " " + input;
     for (int i = 0; i < this.input.length(); i++)
     {
       process(i, this.input.length(), root);
-      System.out.println("TREE-----------------" + i);
-      printTree(root);
+      //System.out.println("TREE-----------------" + i);
+      //printTree(root);
     }
     return root;
   }
@@ -91,7 +100,9 @@ public class NaiveSuffixTree
       }
     }
     // no match
-    node.addChild(new Node(start, end, leaf++));
+    Node newNode = new Node(start, end, leaf++);
+    node.addChild(newNode);
+    nodes.add(newNode);
   }
 
   public int stringMatch(int start, int end, int ostart)
@@ -110,14 +121,16 @@ public class NaiveSuffixTree
   {
     Node newNode = new Node(start + match, end, leaf);
     Node newParent = new Node(start, start + match);
-    newParent.leaves.add(leaf++);
     newParent.leaves.addAll(child.leaves);
+    newParent.leaves.add(leaf++);
     child.stringStart += match;
     parent.children.remove(child);
     parent.children.add(newParent);
     newParent.children.add(newNode);
     newParent.children.add(child);
     newParent.count = child.count + 1;
+    nodes.add(newNode);
+    nodes.add(newParent);
   }
 
   public void printTree(Node node)
@@ -140,24 +153,49 @@ public class NaiveSuffixTree
     }
   }
 
+  public boolean isLeftDiverse(Node node)
+  {
+    if (node.leaves.size() == 0 || node.leaves.get(0) == 1)
+    {
+      return true;
+    }
+    char temp = input.charAt(node.leaves.get(0) - 1 - 1);
+    for (int i = 1; i < node.leaves.size(); i++)
+    {
+      if (temp != input.charAt(node.leaves.get(0) - 1))
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public ArrayList<Node> findMaxRepeats(Node node, int length, int repeats)
+  {
+    ArrayList<Node> returnArr = new ArrayList<Node>();
+    if (node.length() >= length && node.count >= repeats && isLeftDiverse(node) == true)
+    {
+      returnArr.add(node);
+    }
+    for (Node child : node.children)
+    {
+      returnArr.addAll(findMaxRepeats(child, length, repeats));
+    }
+    return returnArr;
+  }
+
   public ArrayList<Integer> findPosition(Node node)
   {
     ArrayList<Integer> returnArr = new ArrayList<Integer>();
     if (node.count == 1)
       returnArr.add(node.leaf);
     return node.leaves;
-/*    for (int i = 0; i < node.children.size(); i++)
-    {
-      if (node.children.get(i).count > 1)
-      {
-        returnArr.addAll(findPosition(node.children.get(i)));
-      }
-      else
-      {
-        returnArr.add(node.children.get(i).leaf);
-      }
-    }
-    return returnArr;*/
+    /*
+     * for (int i = 0; i < node.children.size(); i++) { if
+     * (node.children.get(i).count > 1) {
+     * returnArr.addAll(findPosition(node.children.get(i))); } else {
+     * returnArr.add(node.children.get(i).leaf); } } return returnArr;
+     */
   }
 
   public ArrayList<Integer> findSequence(String sequence, Node node)
@@ -190,8 +228,31 @@ public class NaiveSuffixTree
   public static void main(String[] args)
   {
     NaiveSuffixTree myTree = new NaiveSuffixTree();
-    Node root = myTree.createTree("ATGACTGTAGAGAGATGCAACTGTGCGGACAAC");
-    System.out.println(myTree.findSequence("AGA", root));
+    //String input = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATCGATCGTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTATCGATCGCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCATCGATCGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGATCGATCGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATCGATCGTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTATCGATCGCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCATCGATCGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGATCGATCGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATCGATCGTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTATCGATCGCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCATCGATCGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGATCGATCGAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATCGATCGTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTATCGATCGCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCATCGATCGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGATCGATCG$";
+    String input = "ATCGG$";
+    Node root = myTree.createTree(input);
+    // System.out.println(myTree.nodes);
+    /*
+     * Collections.sort(myTree.nodes, new Comparator<Node>() {
+     * 
+     * @Override public int compare(Node arg0, Node arg1) { // TODO
+     * Auto-generated method stub if(arg0.stringEnd - arg1.stringEnd == 0)
+     * return arg1.count - arg0.count; else return arg0.stringEnd -
+     * arg1.stringEnd; } });
+     */
+   /* for (Node node : myTree.nodes)
+    {
+      System.out.println(node.stringEnd + " " + node.count);
+    }*/
+    //System.out.println(myTree.nodes);
+    // System.out.println(myTree.findMaxRepeats(root, 3));
+    myTree.printTree(root);
+    for (Node node : myTree.findMaxRepeats(root, 1, 2))
+    {
+      System.out.println(input.substring(0, node.stringEnd));
+      System.out.println();
+    }
     // myTree.printTree(root);
+    // Maximal Repeated sequences
   }
 }
